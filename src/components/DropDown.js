e3b7a177e1fbe1a573d233a1764aaf31eb753550
John Daugherty <Johndaugherty1986@gmail.com>
2023-05-16 13:04:11 -0500

    making dropdown toggle to show values on click

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,4 +1,23 @@
-function DropDown() {
-  return <div>dropdown</div>;
+import { useState } from "react";
+function DropDown({ options }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [color, setColor] = useState("");
+  const renderedOptions = options.map((option) => {
+    return <div key={option.value}> {option.label}</div>;
+  });
+  const handleClick = () => {
+    console.log(isOpen);
+    setIsOpen(!isOpen);
+  };
+  const selectOption = () => {};
+  const checkState = () => {
+    console.log(options);
+  };
+  return (
+    <div>
+      <div onClick={handleClick}>Select...</div>
+      {isOpen && <div>{renderedOptions}</div>}
+    </div>
+  );
 }
 export default DropDown;
