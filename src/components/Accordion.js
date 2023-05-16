9af36c5ac651541361e6f83a368cf602b5213a2a
John Daugherty <Johndaugherty1986@gmail.com>
2023-05-15 19:57:00 -0500

    making accordian responsive

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,12 +1,21 @@
 import { render } from "@testing-library/react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+
 import { useState } from "react";
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0);
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
+
+    const icon = (
+      <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+    );
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
+        <div onClick={() => setExpandedIndex(index)}>
+          {icon}
+          {item.label}
+        </div>
         {isExpanded && <div>{item.content}</div>}
       </div>
     );
