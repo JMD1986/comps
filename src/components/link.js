941e21391b62ba51e002a9e2b089b4dd8568f5cc
John Daugherty <Johndaugherty1986@gmail.com>
2023-05-17 12:28:04 -0500

    added metakey, ctrlkey to link component

--- src/components/link.js
+++ src/components/link.js
@@ -3,10 +3,17 @@ import NavigationContext from "../context/navigation";
 function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
   const handleClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
     navigate(to);
   };
   // eslint-disable-next-line jsx-a11y/anchor-is-valid
-  return <a onClick={handleClick}>{children}</a>;
+  return (
+    <a href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
 export default Link;
