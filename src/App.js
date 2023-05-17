148162fc8138ef10925abdab7ac9654d639a5905
John Daugherty <Johndaugherty1986@gmail.com>
2023-05-17 11:25:09 -0500

    making link component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
-
+import Link from "./components/link";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Link to="/accordion">go to according</Link>
+      <Link to="/dropdowngit">go to dropdown</Link>
+    </div>
+  );
 }
 
 export default App;
