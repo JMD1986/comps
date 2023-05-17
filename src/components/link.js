0868c01871203663899fe1f525f052c415c91de5
John Daugherty <Johndaugherty1986@gmail.com>
2023-05-17 12:34:10 -0500

    created and impleneted usenavagation hook

--- src/components/link.js
+++ src/components/link.js
@@ -1,8 +1,7 @@
-import { useContext } from "react";
-import NavigationContext from "../context/navigation";
+import useNavigation from "../hooks/use-navgiation";
 import classNames from "classnames";
 function Link({ to, children }) {
-  const { navigate } = useContext(NavigationContext);
+  const { navigate } = useNavigation();
   const classes = classNames("text-blue-500");
   const handleClick = (event) => {
     if (event.metaKey || event.ctrlKey) {
