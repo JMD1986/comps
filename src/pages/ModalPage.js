7e899405be8fb15756cfdf48bc353e2555b51594
John Daugherty <Johndaugherty1986@gmail.com>
2023-05-18 07:44:48 -0500

    building out modal

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,4 +1,31 @@
+import Modal from "../components/Modal";
+import Button from "../components/Button";
+import { useState } from "react";
 function ModalPage() {
-  return <div>Modal</div>;
+  const [showModal, setShowModal] = useState(false);
+  const handleClick = () => {
+    setShowModal(true);
+  };
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const actionBar = (
+    <div>
+      <Button primary>I Accept</Button>
+    </div>
+  );
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is an important agreement for you to accept</p>
+    </Modal>
+  );
+
+  return (
+    <div>
+      <Button onClick={handleClick}>Open Modal</Button>
+      {showModal && modal}
+    </div>
+  );
 }
 export default ModalPage;
