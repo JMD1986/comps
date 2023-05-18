45af0ff44e39634ce609df0d190eafdc5d0109e2
John Daugherty <Johndaugherty1986@gmail.com>
2023-05-18 09:54:53 -0500

    organizing table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,27 @@
 function Table({ data }) {
-  return <div> {data.length}</div>;
+  const renderedRows = data.map((fruit) => {
+    return (
+      <tr key={fruit.name}>
+        <td>{fruit.name}</td>
+        <td>{fruit.color}</td>
+        <td>{fruit.score}</td>
+      </tr>
+    );
+  });
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Fruit</th>
+            <th>color</th>
+            <th>score</th>
+          </tr>
+        </thead>
+        <tbody>{renderedRows}</tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Table;
