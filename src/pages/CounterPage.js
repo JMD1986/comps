e640a52689a85e4f4d857fed2dea2ee8c40d4536
John Daugherty <Johndaugherty1986@gmail.com>
2023-05-21 15:04:40 -0500

    added decrement and add a lot functionality to counter page

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,12 +1,44 @@
 import Button from "../components/Button";
-import useCounter from "../hooks/use-counter";
+import { useState } from "react";
+import Panel from "../components/Panel";
 function CounterPage({ initialCount }) {
-  const { count, increment } = useCounter(initialCount);
+  const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
+  const handleChange = (event) => {
+    // setValueToAdd(event.target.value);
+    const value = parseInt(event.target.value) || 0;
+    console.log(value);
+    setValueToAdd(value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCount(count + valueToAdd);
+    setValueToAdd(0);
+  };
   return (
-    <div>
-      count is {count}
-      <Button onClick={increment}>increment</Button>
-    </div>
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is {count}</h1>
+      <div className="flex flex-row">
+        <Button onClick={increment}>increment</Button>
+        <Button onClick={decrement}>decrement</Button>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <label>add a lot!</label>
+        <input
+          value={valueToAdd || ""}
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+          type="number"
+          onChange={handleChange}
+        />
+        <Button>add it</Button>
+      </form>
+    </Panel>
   );
 }
 export default CounterPage;
