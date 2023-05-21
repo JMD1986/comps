cce27ec38e62aa111df294716542754d70a4475a
John Daugherty <Johndaugherty1986@gmail.com>
2023-05-21 13:27:42 -0500

    made usecounter a hook

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,24 +1,11 @@
-import { useState, useEffect } from "react";
 import Button from "../components/Button";
-function useSomething(initialCount) {
-  const [count, setCount] = useState(initialCount);
-  useEffect(() => {
-    console.log(count);
-  }, [count]);
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-  return {
-    count,
-    handleClick,
-  };
-}
+import useCounter from "../hooks/use-counter";
 function CounterPage({ initialCount }) {
-  const { count, handleClick } = useSomething(initialCount);
+  const { count, increment } = useCounter(initialCount);
   return (
     <div>
       count is {count}
-      <Button onClick={handleClick}>increment</Button>
+      <Button onClick={increment}>increment</Button>
     </div>
   );
 }
