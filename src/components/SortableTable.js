cdf41ba68669d5b1777f9b8b42e1289c2b54c673
John Daugherty <Johndaugherty1986@gmail.com>
2023-05-21 12:56:05 -0500

    building counterpage

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -7,6 +7,11 @@ function SortableTable(props) {
   const { config, data } = props;
 
   const handleClick = (label) => {
+    if (sortBy && label !== sortBy) {
+      setSortOrder("asc");
+      setSortBy(label);
+      return;
+    }
     if (sortOrder === null) {
       setSortOrder("asc");
       setSortBy(label);
