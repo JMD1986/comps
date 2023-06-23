3d9937f9f270a6da6f5384a3288fce7023678eb6
John Daugherty <Johndaugherty1986@gmail.com>
2023-06-22 19:55:44 -0500

    updated progress bar to take user input

--- src/pages/ProgressBarPage.js
+++ src/pages/ProgressBarPage.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ProgressBar from "../components/ProgressBar";
+import { useState } from "react";
 
 function ProgressBarPage() {
+  const [amount, setAmount] = useState(50);
+  const changeAmount = (event) => {
+    console.log(event.target.value);
+    setAmount(event.target.value);
+  };
   return (
     <div>
-      <ProgressBar amount={50} />
+      <input type="number" min="1" max="100" onChange={changeAmount} />
+      <ProgressBar amount={amount} />
     </div>
   );
 }
